46d4cfc4ae650f29f8ae5669cff82403a3ae272a
Maria Parveen <52498554+livedme@users.noreply.github.com>
2021-02-01 15:54:40 +0600

    update plugin project structure

---
+++ uCookieConsent.Web/App_Plugins/uCookieConsent/backoffice/cookiesAlias/overview.js
@@ -0,0 +1,30 @@
+﻿function openTopTab(evt, item) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(item).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    if ($("#" + item + " .chiltablinks").length > 0)
+        $("#" + item + " .chiltablinks")[0].click();
+}
+
+function openChildTab(evt, item) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("childtabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("chiltablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(item).style.display = "block";
+    evt.currentTarget.className += " active";
+}
